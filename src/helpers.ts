35f8bc843a98006965ecab3c4efe237b82693883
Donovan <donovanjuniogomez@gmail.com>
2022-12-29 03:45:56 -0800

    add likes component

---
+++ src/helpers.ts
@@ -0,0 +1,10 @@
+export function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+export function sleep(ms: number) {
+  return new Promise<void>((resolve) => setTimeout(resolve, ms));
+}
